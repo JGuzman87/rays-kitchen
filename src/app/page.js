84dbac9311a15af5964a1817da35e9bf608ece8c
JGuzman87 <jguzman913@gmail.com>
2025-11-30 10:49:49 -0500

    updated font styles and colors

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,19 @@ import Carousel from "@/components/Carousel";
 import Link from "next/link";
 
 export default function Home() {
-  
+
   return (
     <div className="flex flex-col gap-1">
-      <div className="bg-amber-800/70  flex flex-col gap-8 p-4 backdrop-blur-xs text-white bg-blend-overlay font-mono rounded-2xl">
+      <div className="bg-amber-800/70  flex flex-col gap-8 p-4 backdrop-blur-xs text-white bg-blend-overlay text-2xl font-stretch-75% rounded-2xl">
         <h2 className="text-2xl font-bold p-2 underline">
           Welcome to Ray's Kitchen
         </h2>
-        <p className="text-xl p-2">
+        <p className="text-2xl p-2">
           -- Ray’s Kitchen blends traditional Puerto Rican flavors with a modern
           twist. Fresh ingredients, bold spices, and comfort food vibes.
         </p>
         <p>Check out the the Menu</p>
-        <ul className=" list-disc">
+        <ul className="flex gap-4 underline decoration-solid">
           <li className="hover:underline decoration-solid hover:text-blue-200">
             <Link href={"/menu/lunch"}>Lunch</Link>
           </li>
