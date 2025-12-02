89f8225f3a01e1ccc3fc3e449662084391ede3af
JGuzman87 <jguzman913@gmail.com>
2025-12-02 11:20:08 -0500

    removed uless toast context code

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -1,15 +1,15 @@
 "use client"
 
 import { CountProvider } from "@/context/CountContext";
-import { ToastProvider } from "@/context/ToastContext";
+
 
 
 export default function Providers({children}) {
     return (
-        <ToastProvider>
+
             <CountProvider>
                 {children}
             </CountProvider>
-        </ToastProvider>
+
     )
 }
