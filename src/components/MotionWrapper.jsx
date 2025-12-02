12f27a20b7e41905deebe6e999e7b4d6c397404d
JGuzman87 <jguzman913@gmail.com>
2025-12-02 16:57:03 -0500

    added smother animation for components

--- src/components/MotionWrapper.jsx
+++ src/components/MotionWrapper.jsx
@@ -3,16 +3,14 @@ import { motion, AnimatePresence } from "framer-motion";
 export default function MotionWrapper({ children }) {
   return (
     <AnimatePresence mode="wait">
-      
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: 0.5 }}
-        >
-          {children}
-        </motion.div>
-      
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        {children}
+      </motion.div>
     </AnimatePresence>
   );
 }
