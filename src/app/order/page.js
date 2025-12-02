ebc588bf4c1189bf5cf904b343ee97546f254409
JGuzman87 <jguzman913@gmail.com>
2025-12-01 21:46:32 -0500

    fixed skeleton

--- src/app/order/page.js
+++ src/app/order/page.js
@@ -40,50 +40,55 @@ const Order = () => {
 
   return (
     <div>
-
-      { isLoading ? (<div className="skeleton h-100 md:w-1/2 bg-black/50"></div>) : <div
-        className="card bg-black/50 backdrop-blur-lg md:max-w-1/2 font-stretch-condensed
+     
+{  isLoading ? <div className="skeleton h-100 md:w-1/2  bg-black/30"></div> :      <div
+          className="card bg-black/50 backdrop-blur-lg md:max-w-1/2 font-stretch-condensed
 "
-      >
-        <div className="card-body">
-          <h1 className="font-bold text-2xl text-white ">Review Your Order</h1>
-          <ul className="text-2xl capitalize text-white font-bold ">
-            {storedMeal.length > 0
-              ? storedMeal.map((item, index) => (
-                  <li key={index}>
-                    <p>{item.name}</p>
-                    <p>${item.price}</p>
-                  </li>
-                ))
-              : " No Items Selected"}
-          </ul>
-          <p className="text-white text-2xl font-extrabold">
-            ----------------------------
-          </p>
-          <p className="text-white text-2xl font-bold">
-            Total Price : $
-            {storedMeal
-              .reduce((total, item) => total + item.price, 0)
-              .toFixed(2)}
-          </p>
-          <div className="card-actions justify-end">
-            <button
-              type="button"
-              className="btn btn-success"
-              onClick={handleClick}
-            >
-              Submit Order
-            </button>
-            <button
-              type="button"
-              className="btn btn-error"
-              onClick={handleRemove}
-            >
-              Restart Order
-            </button>
+        >
+          <div className="card-body">
+            <h1 className="font-bold text-2xl text-white ">
+              Review Your Order
+            </h1>
+            
+              <ul className="text-2xl capitalize text-white font-bold ">
+                {storedMeal.length > 0
+                  ? storedMeal.map((item, index) => (
+                      <li key={index}>
+                        <p>{item.name}</p>
+                        <p>${item.price}</p>
+                      </li>
+                    ))
+                  : " No Items Selected"}
+              </ul>
+            
+            <p className="text-white text-2xl font-extrabold">
+              ----------------------------
+            </p>
+            <p className="text-white text-2xl font-extrabold">
+              Total Price : $
+              {storedMeal
+                .reduce((total, item) => total + item.price, 0)
+                .toFixed(2)}
+            </p>
+            <div className="card-actions justify-end">
+              <button
+                type="button"
+                className="btn btn-success"
+                onClick={handleClick}
+              >
+                Submit Order
+              </button>
+              <button
+                type="button"
+                className="btn btn-error"
+                onClick={handleRemove}
+              >
+                Restart Order
+              </button>
+            </div>
           </div>
-        </div>
-      </div>}
+        </div>}
+      
     </div>
   );
 };
